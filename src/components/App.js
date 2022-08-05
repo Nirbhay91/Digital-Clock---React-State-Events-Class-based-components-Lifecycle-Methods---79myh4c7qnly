dc43f0bf2c318fc714c351c0d2015cc793151b36
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-08-05 09:23:56 +0530

    update file

--- src/components/App.js
+++ src/components/App.js
@@ -56,26 +56,3 @@ export default App;
 
 
 
-// import React, { useState, useEffect } from "react";
-// import "../styles/App.css";
-// const App = () => {
-//   const [currentTime, setCurrentTime] = useState();
-
-//   useEffect(() => {
-//     const id = setInterval(
-//       () => setCurrentTime(new Date().toLocaleString()),
-//       1000
-//     );
-//     return () => {
-//       clearInterval(id);
-//     };
-//   }, []);
-
-//   return (
-//     <div id="main">
-//       <div className="date-time">{currentTime}</div>
-//     </div>
-//   );
-// };
-
-// export default App;
