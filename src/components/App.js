1b3d6d26ac5bbfbf98ddcfcf8f2657019615c476
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-08-05 22:36:31 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,23 @@
-import React, { Component, useState, useEffect } from 'react';
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
 
-function getCurrentTime() {
-  const date = new Date();
-  let hours = date.getHours();
-  let amOrPm = 'AM';
-
-  if (isPM(hours)) {
-    amOrPm = 'PM';
-    hours = hours - 12;
-  }
-  const mins = date.getMinutes().toString().padStart(2, '0');
-  const secs = date.getSeconds().toString().padStart(2, '0');
-
-  return `${hours}:${mins}:${secs} ${amOrPm}`;
-}
-
-function isPM(hours) {
-  if (hours > 12) {
-    return true;
-  }
-  return false;
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: getCurrentTime() };
-  }
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
-      this.setState({ time: getCurrentTime() });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-  render() {
-    return (
-      <div className="Clock">
-        <h3 id="time">{this.state.time}</h3>
-        
-        
-      </div>
+  useEffect(() => {
+    const id = setInterval(
+      () => setCurrentTime(new Date().toLocaleString()),
+      1000
     );
-  }
-}
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
-// function Clock() {
-//   useEffect(function () {}, []);
-// }
+  return (
+    <div id="main">
+      <div className="date-time">{currentTime}</div>
+    </div>
+  );
+};
 
 export default App;
-
-
-
